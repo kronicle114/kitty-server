6d17887a1c051c8af047961d84a330ad97d4dc59
Trisha Aguinaldo <trisha.aguinaldo@gmail.com>
2019-06-01 10:47:07 -0400

    added example schema & docket-compose.yml

---
+++ __main__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, print_function
+
+# As an authenticated user I want to favorite an github open source project
+# As an authenticated user I want to unfavorite an github open source project
+# As an authenticated user I want to list all bookmarked github open source projects I’ve previously favorited
+
+# For the authenticated user, fetches all favorited github open source projects
+#GET /kudos
+
+# Favorite a github open source project for the authenticated user
+#POST /kudos
+
+# Unfavorite a favorited github open source project
+#DELETE /kudos/:id
